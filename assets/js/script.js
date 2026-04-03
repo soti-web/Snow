99023d4cce0ae57f80304bbda52c79f878ac68da
soti-web <soti03e250058@epu.edu.iq>
2026-04-03 14:56:13 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+// Menu Toggle
+const menuBtn = document.querySelectorAll('.btn-icon')[1];
+const mobileMenu = document.getElementById('mobileMenu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('open');
+});
+
+// Theme Toggle
+const themeBtn = document.getElementById('themeBtn');
+
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('light');
+
+  if (document.body.classList.contains('light')) {
+    themeBtn.textContent = '🌙';
+  } else {
+    themeBtn.textContent = '☀️';
+  }
+});
 
